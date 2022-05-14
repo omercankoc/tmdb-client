684852572dfe3784375aeb5587452b66e3e90cd4
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-05-14 13:37:45 +0300

    .

---
+++ tmdbclient/tmdbclient/Model/Movie+Stub.swift
@@ -0,0 +1,8 @@
+//
+//  Movie+Stub.swift
+//  tmdbclient
+//
+//  Created by Ömer Can Koç on 7.05.2022.
+//
+
+import Foundation
---
+++ tmdbclient/tmdbclient/tmdbclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  tmdbclientApp.swift
+//  tmdbclient
+//
+//  Created by Ömer Can Koç on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct tmdbclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
